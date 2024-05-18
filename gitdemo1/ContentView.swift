4d9ef42b2f5b1a0eb9781dfe9747a5490333f53d
Rana Shahzaib <shahzaibrana0141@gmail.com>
2024-05-18 15:18:44 +0500

    Initial Commit

---
+++ gitdemo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitdemo1
+//
+//  Created by apple on 5/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitdemo1/gitdemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemo1App.swift
+//  gitdemo1
+//
+//  Created by apple on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct gitdemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
